f7300a067a6a67cbe9af194cc1a6ef87c397e2e3
fahad0samarar <fah8ad@outlook.com>
2024-02-20 20:49:24 +0200

    updata the viewmodel

---
+++ Calorie Master/Helper/TimeFormating.swift
@@ -0,0 +1,8 @@
+//
+//  TimeFormating.swift
+//  Calorie Master
+//
+//  Created by fahad samara on 2/20/24.
+//
+
+import Foundation
---
+++ Calorie Master/Views/EditFoodView.swift
@@ -0,0 +1,18 @@
+//
+//  EditFoodView.swift
+//  Calorie Master
+//
+//  Created by fahad samara on 2/20/24.
+//
+
+import SwiftUI
+
+struct EditFoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditFoodView()
+}
