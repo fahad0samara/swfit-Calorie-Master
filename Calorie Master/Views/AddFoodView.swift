88db12c29b50fc68ae33c48fbcf2ef77fa16e2c5
fahad0samarar <fah8ad@outlook.com>
2024-02-20 14:25:23 +0200

    add mvvm

---
+++ Calorie Master/Views/AddFoodView.swift
@@ -0,0 +1,18 @@
+//
+//  AddFoodView.swift
+//  Calorie Master
+//
+//  Created by fahad samara on 2/20/24.
+//
+
+import SwiftUI
+
+struct AddFoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddFoodView()
+}
