619683e91235d62c9193a9fbd19f25744850ce49
fahad0samarar <fah8ad@outlook.com>
2024-02-20 11:51:35 +0200

    Initial Commit

---
+++ Calorie Master/Calorie_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_MasterApp.swift
+//  Calorie Master
+//
+//  Created by fahad samara on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
